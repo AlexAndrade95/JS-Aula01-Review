0c14e74df52fdbf3d8f5ab7262e5b419080c62d8
AlexAndrade95 <alexneves19955@gmail.com>
2023-06-23 20:20:36 -0300

    Bloco de código

--- Class01/01 - Introduction.js
+++ Class01/01 - Introduction.js
@@ -1,18 +1,18 @@
-// Class01 - Introduction 
-// Por que estudar/aprender JavaScript/EcmaScript?
-// JS é uma linguagem muito versátil, 
-// podendo ser usada em diferentes cenários web(Frontend/backend)
-// mobile(React Native)
-// e desktop também
+/* Class01 - Introduction 
+ Por que estudar/aprender JavaScript/EcmaScript?
+ JS é uma linguagem muito versátil, 
+ podendo ser usada em diferentes cenários web(Frontend/backend)
+ mobile(React Native)
+ e desktop também
 
-//Ferramentas usadas:
-// Node - baixar a versão LTS(versão que tem o maior suporte)
-// Vscode - editor de texto(muito completo)
-// plugin do vscode: 
-// code runner (permite executar código JS diretamente no VScode)
+ //Ferramentas usadas:
+ Node - baixar a versão LTS(versão que tem o maior suporte)
+ Vscode - editor de texto(muito completo)
+ plugin do vscode: 
+ code runner (permite executar código JS diretamente no VScode)
 
 // Atalhos VScode
-// ctrl+k+c = comentario 
-// ctrl+alt+n = run (executa o código)
+ ctrl+k+c = comentario 
+ ctrl+alt+n = run (executa o código)
 
-
+*/
---
+++ Class01/04 - Bloco de codigo.js
@@ -0,0 +1,18 @@
+/*Bloco de código
+  Bloco em JS é definido por par de chaves {}
+*/
+// exemplos:
+{
+    console.log("Bloco01")
+}
+
+{
+    console.log("Bloco02")
+}
+
+//Pode-se ter bloco dentro de bloco
+{
+    {
+        console.log("Bloco03")
+    }
+}
