86c3ec1b57ba027d709798dc46c73cd6f3326763
AlexAndrade95 <alexneves19955@gmail.com>
2023-06-22 22:30:16 -0300

    01 - Introduction

--- Aula01.js
+++
@@ -1,5 +0,0 @@
-//Aula 01
-// Por que estudar JavaScript?
-// JS é uma linguagem muito versátil, podenser usada em cenários web(Frontend/backend)
-// e mobile(React Native)
-
---
+++ Class01/02 - Progamming01.js
@@ -0,0 +1,13 @@
+//Progamming 01
+
+// Setença de código
+// unidade básica de c[odigo (como Frases/parágrafos em livros)
+// o ";" no final é opcional
+// pode-se ocupar várias linhas
+// exemplo:
+console.log("Bom dia!")
+console.log("Boa tarde!");
+console.log("Boa noite!")
+
+// Bloco de código
+// Agrupamento de setenças de códigos
