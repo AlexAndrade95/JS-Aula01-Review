8ea2141b8fe5c3a716515171a139799749018784
AlexAndrade95 <alexneves19955@gmail.com>
2023-06-23 19:57:19 -0300

    Setença de código

---
+++ Class01/Setença.js
@@ -0,0 +1,12 @@
+// Setença de código
+// unidade básica de codigo (como Frases/parágrafos em livros)
+// o ";" no final é opcional
+// pode-se ocupar várias linhas
+// a ordem importa
+// exemplo:
+console.log("Bom dia!") //Para exibir a mensagem na tela
+console.log('Boa tarde!');
+console.log("Boa noite!")
+
+// Bloco de código
+// Agrupamento de setenças de códigos
