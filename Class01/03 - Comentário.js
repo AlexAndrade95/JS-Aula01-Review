eff234fe5cbf9773eb39b425d220ea5ea996de53
AlexAndrade95 <alexneves19955@gmail.com>
2023-06-23 20:06:24 -0300

    Comentário

---
+++ Class01/03 - Comentário.js
@@ -0,0 +1,14 @@
+// Comentario
+
+// Usado para orientar a funcionalidae do código
+// Em projetos reais usar comentários  somente como for necessário
+// exemplo:
+console.log("Teste1") //um exemplo de comentário
+
+// Formas de comentário em JS:
+// Comentário de uma linha
+/*
+    Esse é um comentário 
+    de mútiplas
+    linhas!
+*/
