b44048699a007894d7f0b80b0890b69370b2d3fb
AlexAndrade95 <alexneves19955@gmail.com>
2023-06-27 15:08:45 -0300

    01 - Algoritmo

---
+++ Class 02 - JS Fundamentos/01 - Algoritmo.js
@@ -0,0 +1,20 @@
+/* 
+  Algoritmo
+ é uma sequência de passos (como uma receita de bolos)
+ a ordem dos passos é importante
+ podendo ter repetição ou não
+ pode-se ter tomadas de decisão (if/else)
+ possue entrada e saída(dados e resultados)
+ pode ter entrada e não ter saída
+ ou até mesmo não ter entrada e ter saída (hora certa)
+
+ Representação de Algoritmo:
+ -Fluxogramas
+ -Linguagem natural
+ -Linguagem artificial (C, C#, Java, JavaScript...)
+ -Pseudo-Linguagem (Portugol)
+
+ Alto nível - Mais proximo do Programador(C, C#, Java, JavaScript...)
+ Baixo nível - mais proximo do Computador(Assembly)
+ 
+ */
