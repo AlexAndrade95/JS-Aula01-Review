2071956af6a1202b06861ea8e838c64485712e45
AlexAndrade95 <alexneves19955@gmail.com>
2023-06-27 15:42:33 -0300

    03 - Organizacao de codigo

---
+++ Class 02 - JS Fundamentos/03 - Organizacao basica.js
@@ -0,0 +1,15 @@
+// Organização basica de código JS
+// Organizado por setença/blocos de código
+// exemplos:
+console.log("Setença de código") 
+
+{
+    console.log("Bloco de código") //bloco pois está agrupado por chaves {}
+}
+
+{
+    {
+        console.log("Bloco dentro de outro")
+    }
+}
+
