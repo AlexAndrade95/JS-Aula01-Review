65409a07986c5e739b23e98cd4252c97b08b8462
AlexAndrade95 <alexneves19955@gmail.com>
2023-06-27 15:31:33 -0300

    02 - Estrutura de dados

---
+++ Class 02 - JS Fundamentos/02 - Estrutura de dados.js
@@ -0,0 +1,18 @@
+/*
+Estrutura de dados
+oganiza/administrar os dados
+exemplos: lista, pilha, fila, arvore(sistema de pastas do computador)
+          tabela
+
+Estrutura de dados clássicas: INT, REAL, String, Boolean
+
+Variáveis e Constantes
+serve para armazenar valores
+
+Operações
+Atribuição(=), Aritmetica(+ - * /), Relacional(equal notequal),
+Lógicas(and or)
+
+Estruturas personalizadas
+criar novas estruturas de acordo com a necessidade do Projeto/cliente
+*/
