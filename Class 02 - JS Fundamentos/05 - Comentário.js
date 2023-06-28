18d5bae4a3f89002e01e9aa4070b3ac5f06c1e5f
AlexAndrade95 <alexneves19955@gmail.com>
2023-06-28 14:30:10 -0300

    05 - Comentário

---
+++ Class 02 - JS Fundamentos/05 - Comentário.js
@@ -0,0 +1,9 @@
+// Comentário
+// para uma linha usa-se barra barra (//)
+
+/*Comentário de
+  múltiplas linhas  
+*/
+
+// usa-se para explicar o que não está legível no código
+// (dê preferência para um código legível)
