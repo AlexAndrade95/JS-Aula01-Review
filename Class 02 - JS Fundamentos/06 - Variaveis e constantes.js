f7d0474a1e4e4d8c8dc6cb5185a3c802c2cd638b
AlexAndrade95 <alexneves19955@gmail.com>
2023-06-29 17:19:03 -0300

    06 - Variaveis e Constantes

---
+++ Class 02 - JS Fundamentos/06 - Variaveis e constantes.js
@@ -0,0 +1,11 @@
+// Variáveis
+var a = 3 // "var" é uma palavra reservada para variável
+          // "a" é o identificador da variável
+let b = 4 // let também é uma palavra reservada para variável
+          // "b" é o identificador da variável
+
+// obs é preferível usar let para declarar variáveis
+
+// Constantes
+const c = 5 // "const" é uma palavra reservada para constante
+            // "c" é o identificador da constante 
