781c511f68052dc05ba6c82cd5c0f2764ed5480c
AlexAndrade95 <alexneves19955@gmail.com>
2023-06-29 17:24:08 -0300

    07 - Tipagem fraca

---
+++ Class 02 - JS Fundamentos/07 - Tipagem Fraca.js
@@ -0,0 +1,11 @@
+// Tipagem Fraca
+// Javascript é uma linguagem fracamente tipada, 
+// pois uma  mesma variável pode usar diferentes tipagens
+// Exemplo
+let qualquer = "legal"
+console.log(qualquer)
+console.log(typeof qualquer) //Tipo String
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer) //Tipo number
