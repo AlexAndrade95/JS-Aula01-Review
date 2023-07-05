283fd5a8a331e6322a68209fc5c706caef01a362
AlexAndrade95 <alexneves19955@gmail.com>
2023-07-05 20:50:24 -0300

    16 - Null&Undefined

---
+++ Class 02 - JS Fundamentos/16 - Null&Undefined.js
@@ -0,0 +1,23 @@
+// Null e undefined
+
+let valor //não inicializada
+console.log(valor) //undefined
+
+valor = null //ausência de valor
+console.log(valor) //null
+// console.log(valor.toString)//Erro
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto) //{}
+
+produto.preco = 3.50
+console.log(produto)
+
+produto.preco = undefined
+console.log(!!produto.preco) //false
+console.log(produto) //{ preco: undefined }
+
+produto.preco = null // sem preco
+console.log(!!produto.preco) //false
+console.log(produto) //{ preco: null }
