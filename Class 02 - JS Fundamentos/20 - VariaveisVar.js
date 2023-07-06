dea04ef856109cb336079c7eb36bb02089fdb5e7
AlexAndrade95 <alexneves19955@gmail.com>
2023-07-06 20:37:17 -0300

    20 - VariaveisVar

---
+++ Class 02 - JS Fundamentos/20 - VariaveisVar.js
@@ -0,0 +1,20 @@
+// Variáveis com VAR
+
+{
+    {
+        {
+            var sera = 'Será???'
+            console.log(sera) //Será???
+        }
+    }
+}
+
+console.log(sera) //Será???
+
+function teste() {
+    var local = 123
+    console.log(local) ///123
+}
+
+teste()
+// console.log(local) ///erro
