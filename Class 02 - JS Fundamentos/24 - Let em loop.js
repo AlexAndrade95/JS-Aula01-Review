321a4251efaf724989edaaa9e6255980930dba99
AlexAndrade95 <alexneves19955@gmail.com>
2023-07-11 20:41:16 -0300

    24 - Let em loop

---
+++ Class 02 - JS Fundamentos/24 - Let em loop.js
@@ -0,0 +1,10 @@
+// Let em loop
+
+for (let i = 0; i < 10; i++){
+    console.log(i)
+}
+// 0 1 2 3 4 5 6 7 8 9 
+
+// console.log('i = ', i) 
+//usando let apresentará erro, pois let tem escopo de bloco, logo
+//não permite ser usada fora do escopo do bloco
