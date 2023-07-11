a74ce78e1bfb99dc5302a6737f15ac193aeeaf34
AlexAndrade95 <alexneves19955@gmail.com>
2023-07-11 20:36:41 -0300

    23 - Var em loop

---
+++ Class 02 - JS Fundamentos/23 - Var em loop.js
@@ -0,0 +1,11 @@
+//Var em Loop
+
+for (var i = 0; i < 10; i++){
+    console.log(i)
+}
+// 0 1 2 3 4 5 6 7 8 9 
+
+console.log('i =', i)
+//i = 10 
+//pois como var não tem escopo de bloco, fora do loop a variável i será igual 
+//ao valor em que "i" saiu do loop
