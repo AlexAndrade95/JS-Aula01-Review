0a7640ea9e7e8c91b7eda1eb11a75afeb27ad375
AlexAndrade95 <alexneves19955@gmail.com>
2023-07-11 20:21:46 -0300

    21 - VariaveisVar2

---
+++ Class 02 - JS Fundamentos/21 - VariaveisVar2.js
@@ -0,0 +1,9 @@
+var numero = 1
+{
+    var numero = 2
+    console.log('dentro =', numero)
+}
+console.log('fora =', numero)
+//dentro = 2
+//fora = 2
+//aparecerá 2 duas vezes, pois a variável VAR NÃO TEM ESCOPO DE BLOCO!!!
