0da7117ddef1b3a5c1ad3204f1e85929912b1c61
AlexAndrade95 <alexneves19955@gmail.com>
2023-07-18 21:15:56 -0300

    27 - Hosting

---
+++ Class 02 - JS Fundamentos/27 - Hosting.js
@@ -0,0 +1,15 @@
+// Hosting - Içamento
+// não é recomendado usar, pois pode causar confusão na leitura do código por outra pessoa
+
+console.log('a =', a)
+var a = 2
+console.log('a =', a)
+// a = undefined
+// a = 2
+//ob: aconteceu o hosting, pois var tem o conceito de hosting
+
+
+// console.log('a =', a)
+// let a = 2
+// console.log('a =', a)
+//obs: acontecerá um erro, pois let não tem o conceito de hosting
