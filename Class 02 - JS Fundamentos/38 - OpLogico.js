36ed00f687de37f4434d63f275f1369c079e582d
AlexAndrade95 <alexneves19955@gmail.com>
2023-07-25 20:58:04 -0300

    38 - OpLogico 1.0

---
+++ Class 02 - JS Fundamentos/38 - OpLogico.js
@@ -0,0 +1,17 @@
+// true  and true = true
+// true  and false = false
+// false and ? = false
+
+// true  or ? = true
+// false or true = true
+// false or false = false
+
+// ou exclusivo
+// true  xor true = false
+// false xor false = false
+// true  xor false = true
+// false xor true = true
+
+// negação lógica
+// !true  = false 
+// !false = true
