2c2c287fc4d2f775eb2d32d008292c61f55b53de
AlexAndrade95 <alexneves19955@gmail.com>
2023-07-29 20:57:45 -0300

    42 - Browser x Node 1.2

--- Class 02 - JS Fundamentos/42 - Browser x Node.js
+++ Class 02 - JS Fundamentos/42 - Browser x Node.js
@@ -1,6 +1,12 @@
 //Browser
 // window - usado para contexto glogal
 // declarar variáveis com var vai para o window
+// declarar com let não irá para o window (mais seguro para usar)
+// declarar com const tbm não irá para o window (mais seguro para usar)
 
 //Node
 // this - usado para contexto global
+// global = window
+// module.exports = this
+
+// dica: fuja do escopo global!!!!!
