4a9353f4bb6acc2bf4dc6930428632eac7977cb3
AlexAndrade95 <alexneves19955@gmail.com>
2023-08-08 19:50:10 -0300

    05 - this

---
+++ Class 04 - Função/05 - this.js
@@ -0,0 +1,4 @@
+// this 
+// pode variar, apontando para contexto Global ou não
+// varia de acordo com a forma que chama o this na função
+// obs: na arrow function o this NÂO varia (nesse caso sempre será global)
