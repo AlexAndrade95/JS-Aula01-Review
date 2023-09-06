b303ef89994bf3d9842c44650eecb95f26c583f3
AlexAndrade95 <alexneves19955@gmail.com>
2023-09-06 19:23:26 -0300

    01 - OO

---
+++ Class 05 - Objetos/01 - OO.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL!!
+
+// Procedural (exemplo linguagem C)
+processamento(valor1, valor2, valor3)
+
+// OO (exemplo linguagem JS)
+Objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento()
+
+// Princípios Importantes
+// 1 - abstracao
+// 2 - encapsulamento 
+// 3 - herança (prototype) - recebe-se atributos e comportamentos de uma estrutura "pai"
+// 4 - polimorfismo - multiplas formas
