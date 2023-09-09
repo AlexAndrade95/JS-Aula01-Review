1e01d46802754c01d923b655d135dd15c33f2381
AlexAndrade95 <alexneves19955@gmail.com>
2023-09-09 15:46:06 -0300

    05 - NotacaoLiteral

---
+++ Class 05 - Objetos/05 - NotacaoLiteral.js
@@ -0,0 +1,36 @@
+const a = 1
+const b = 2
+const c = 3
+
+// antes
+const obj1 = { a: a, b: b, c: c }
+// agora 
+const obj2 = { a, b, c }
+console.log(obj1, obj2)
+//{ a: 1, b: 2, c: 3 } { a: 1, b: 2, c: 3 }
+
+const nomeAttr = 'nota'
+const valorAttr = 7.87
+
+const obj3 = {}
+obj3[nomeAttr] = valorAttr
+console.log(obj3)
+//{ nota: 7.87 }
+
+const obj4 = {[nomeAttr]: valorAttr }
+console.log(obj4)
+//{ nota: 7.87 }
+
+const obj5 = {
+    // antes
+    funcao1: function() {
+        //...
+    },
+    // depois
+    funcao2() {
+        //...
+    }
+}
+
+console.log(obj5)
+//{ funcao1: [Function: funcao1], funcao2: [Function: funcao2] }
