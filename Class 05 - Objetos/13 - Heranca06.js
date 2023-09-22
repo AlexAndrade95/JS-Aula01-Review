73a967a0c87eb0fb637847eac58e9fc6bf85c167
AlexAndrade95 <alexneves19955@gmail.com>
2023-09-22 14:31:53 -0300

    13 - Heranca06

---
+++ Class 05 - Objetos/13 - Heranca06.js
@@ -0,0 +1,22 @@
+function Aula(nome, videoID){
+    this.nome = nome
+    this.videoID = videoID
+}
+
+const aula1 = new Aula('Bem vindo', 123)
+const aula2 = new Aula('Até Breve', 456)
+console.log(aula1, aula2)
+//Aula { nome: 'Bem vindo', videoID: 123 } Aula { nome: 'Até Breve', videoID: 456 }
+
+// simulando o new
+function novo(f, ...params) {
+    const obj = {}
+    obj.__proto__ = f.prototype
+    f.apply(obj, params)
+    return obj
+}
+
+const aula3 = novo('Bem vindo', 123)
+const aula4 = novo('Até Breve', 456)
+console.log(aula3, aula4)
+////Aula { nome: 'Bem vindo', videoID: 123 } Aula { nome: 'Até Breve', videoID: 456 }
